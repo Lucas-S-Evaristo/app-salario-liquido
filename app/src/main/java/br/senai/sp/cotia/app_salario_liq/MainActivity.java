22f8ab350e6b0db345f21e80630b5f31e82815ee
Lucas Evaristo <lucasev35@gmail.com>
2022-02-15 14:05:29 -0300

    Commit 1

---
+++ app/src/main/java/br/senai/sp/cotia/app_salario_liq/MainActivity.java
@@ -0,0 +1,14 @@
+package br.senai.sp.cotia.app_salario_liq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
